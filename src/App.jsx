b38926f3ee478b060bedc02e88f4d7d0cd410a5e
tf-souli <safa.souli@tf-teleconception.com>
2023-06-16 15:11:25 +0100

    feat: add scss to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/images/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './assets/fonts/made-tommy-soft/stylesheet.css'
+import './assets/fonts/marcellus/stylesheet.css'
+import './assets/scss/style.scss'
 
 function App() {
   const [count, setCount] = useState(0)
